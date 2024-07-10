88e22fc3c3c4655dede20dbb26a65f1b272ed9d8
Vishva066 <vishvaprasad.p.2019.cse@rajalakshmi.edu.in>
2024-07-10 23:24:35 +0530

    Authorization middleware for application router

--- Backend_Day4/Express-Middleware/Application_Level_Middleware/application2.js
+++ Backend_Day4/Express-Middleware/Application_Level_Middleware/application2.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+const authMiddleware = (req, res, next) => {
+    const isAuthenticated = false; // Replace with your actual authentication logic
+  
+    if (isAuthenticated) {
+      next(); // If authenticated, proceed to the next middleware or route handler
+    } else {
+      res.status(401).send('Unauthorized');
+    }
+  };
+
+app.use(authMiddleware);
+
 app.get('/data',(req,res) => {
     res.send('Hello World')
 })
