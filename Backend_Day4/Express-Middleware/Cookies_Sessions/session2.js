9624caf04b001fe2eb96e1bf8e1243a6bf6a434e
Vishva066 <vishvaprasad.p.2019.cse@rajalakshmi.edu.in>
2024-07-12 08:58:12 +0530

    session code update

--- Backend_Day4/Express-Middleware/Cookies_Sessions/session2.js
+++ Backend_Day4/Express-Middleware/Cookies_Sessions/session2.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 // Configure session middleware
 app.use(session({
