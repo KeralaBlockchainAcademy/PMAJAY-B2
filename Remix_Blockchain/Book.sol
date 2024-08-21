b23de87d0f2cabb46d121acc04fd23101e6971c0
pglekshmi <pglekshmi88@gmail.com>
2024-08-21 12:34:13 +0530

    updated

--- Remix_Blockchain/Book.sol
+++ Remix_Blockchain/Book.sol
@@ -4,8 +4,11 @@ pragma solidity 0.8.23;
 
 contract Book{
 
-    string  title;
-    uint price;
+    string public  title;
+    uint public price;
+    address public owner;
+    bool sold;
+    
 
     function setBook(string memory x,uint y)public  {
         title=x;
@@ -15,6 +18,23 @@ contract Book{
     function getBook()public view returns(string memory,uint){
         return (title,price);
     }
+
+    function toWei(uint amt)public pure  returns(uint){
+        return (amt*1000000000000000000);
+    }
+
+    function buyBook()public payable {
+        //5<=7000000000000000000
+        if(toWei(price)<=msg.value){
+            owner=msg.sender;
+            sold=true;
+            uint bal=msg.value-toWei(price); 
+            if(bal>0){
+               payable (msg.sender) .transfer(bal);
+            }
+        }
+    }
+
 
 
 
